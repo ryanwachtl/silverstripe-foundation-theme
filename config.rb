08ef57deaace57f2469f824ef32ef3caf435d88d
Ryan Wachtl <ryan.wachtl@gmail.com>
2013-01-02 13:36:28 -0600

    Removed form classes from editor.css

--- config.rb
+++ config.rb
@@ -1,7 +1,6 @@
 require 'zurb-foundation'
 # Require any additional compass plugins here.
 
-
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
@@ -18,10 +17,3 @@ relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
-
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
