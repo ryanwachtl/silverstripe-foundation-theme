c0d9f8eb19222f4c5eb6e1b38c110b9d00b8b26f
Ryan Wachtl <ryan.wachtl@gmail.com>
2014-03-06 14:35:08 -0600

    Initialize the Joyride demo on the KitchenSink demo

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,8 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+// trigger for joyride demo in KitchenSink demo
+$('#start-jr').on('click', function() {
+	$(document).foundation('joyride','start');
+});
